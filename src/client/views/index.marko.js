6403ebb457f9a8f24582b64faaac4485704db525
Ezequiel Gomez <ezequiel.gomez@softonic.com>
2019-03-27 17:47:04 +0100

    add - MarkoJs template engine
    Implemented markojs as a template engine for hapijs.
    Implemented the main route: '/'

---
+++ src/client/views/index.marko.js
@@ -0,0 +1,25 @@
+// Compiled using marko@4.7.5 - DO NOT EDIT
+"use strict";
+
+var marko_template = module.exports = require("marko/src/html").t(__filename),
+    marko_componentType = "/bots-finder$1.0.0/src/client/views/index.marko",
+    components_helpers = require("marko/src/components/helpers"),
+    marko_renderer = components_helpers.r,
+    marko_defineComponent = components_helpers.c;
+
+function render(input, out, __component, component, state) {
+  var data = input;
+
+  out.w("<h1>Hello Chats Bots</h1>");
+}
+
+marko_template._ = marko_renderer(render, {
+    ___implicit: true,
+    ___type: marko_componentType
+  });
+
+marko_template.Component = marko_defineComponent({}, marko_template._);
+
+marko_template.meta = {
+    id: "/bots-finder$1.0.0/src/client/views/index.marko"
+  };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,56 @@
 import 'dotenv/config';
 
-console.log(process.env.PORT)
+import * as Hapi from 'hapi';
+import * as Vision from 'vision';
+import * as Marko from 'marko';
 
-// const Hapi = require('hapi');
-//
-// const index = new Hapi.Server();
+const server = new Hapi.Server();
+
+server.connection({
+    port: process.env.PORT,
+    host: 'localhost'
+});
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply.view('index');
+    }
+})
+
+
+async function init() {
+    try {
+        await server.register(Vision);
+        server.views({
+            engines: {
+                marko: {
+                    compile(src, templateOptions) {
+                        const template = Marko.load(templateOptions.filename, src);
+                        return (context) => {
+                            try {
+                                return template.renderToString(context);
+                            } catch (error) {
+                                const { requestId } = context;
+                                serverLogger.error({ error, requestId }, 'Error rendering template');
+                                throw error;
+                            }
+                        };
+                    },
+                },
+            },
+            relativeTo: __dirname,
+            path: 'client/views',
+        });
+        await server.start();
+    } catch(err) {
+        throw err;
+    }
+
+    console.log('Server started at:', server.info.uri);
+}
+
+init().catch(function (err) {
+    console.error('err', err);
+});
